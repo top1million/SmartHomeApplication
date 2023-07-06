1c20a9bf57ecfd62b1720cb67bed0410e1dad848
top1million <1180620@student.birzeit.edu>
2023-07-06 14:24:27 +0300

    fixed structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoWidth: true,
+  items: 4,
+});
