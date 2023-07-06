0233482afba75152396b2685221f81a2b97ddc75
top1million <1180620@student.birzeit.edu>
2023-07-06 16:18:22 +0300

    hello its me

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 60,
+  nav: true,
+  autoWidth: true,
+  items: 2,
+  navText: [
+    "<img src='assets/images/vectors/left.png'>",
+    "<img src='assets/images/vectors/right.png'>",
+  ],
+});
