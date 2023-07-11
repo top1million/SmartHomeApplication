2f7bc72cb20bcb6c524de9910af6a1c27f2a60f2
top1million <1180620@student.birzeit.edu>
2023-07-11 13:39:05 +0300

    hello its me

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,15 @@ $(".owl-carousel").owlCarousel({
 });
 
 $(document).ready(function () {
-  $("div.question i.fa-plus").click(function () {
-    $("div.question p").toggle();
-    $(this).toggleClass("fa-plus fa-minus");
+  $(".question").click(function () {
+    //hide all other paragraphs and remove active class and change icon
+    $(".question").not(this).find("p").hide();
+    $(".question").not(this).removeClass("active");
+    $(".question").not(this).find("i").removeClass("fa-solid fa-minus fa-3x").addClass("fa-solid fa-plus fa-3x");
+    $(this).find("p").slideToggle();
+    //change background using active clasee
+    $(this).toggleClass("active");
+    $(this).find("i").toggleClass("fa-solid fa-plus fa-3x fa-solid fa-minus fa-3x");
+    
   });
 });
