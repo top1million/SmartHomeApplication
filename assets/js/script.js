3aff29147b1879bf9ab6fab9fea47600087c48f9
top1million <1180620@student.birzeit.edu>
2023-07-11 10:30:30 +0300

    hello

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 60,
   nav: true,
-  autoWidth: true,
-  items: 2,
+  autoWidth: false,
+  items:2,
   navText: [
     "<img src='assets/images/vectors/left.png'>",
     "<img src='assets/images/vectors/right.png'>",
